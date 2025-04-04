d3dafeb0c494b9a697d6feb3320a6b4fa7662784
Your Name <samuelishimw02@gmail.com>
2025-04-05 01:12:57 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
 const Home=()=>{
     return (
-        <div className="all images">
+        <div>
+             <h2>Welcome to our shop</h2>
+        <div className="allimages">
                <img src="b1.jpg" alt="" />
+               <img src="b3.jpg" alt="" />
+               
+               <img src="blog-img1" alt="" />
+               
+        </div>
         </div>
     );
 }
